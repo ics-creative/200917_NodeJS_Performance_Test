5e1ccbebcfbcb08e4d48ae84dc25d3667e939e2a
ics-ikeda <ikeda@ics-web.jp>
2020-09-14 18:41:43 +0900

    複数回テストを利用可能に

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
 const {execSync} = require('child_process');
 const os = require('os');
 
-console.log('start performance...');
+const STEP = 5;
 
-const platform = {
-  cpu: os.cpus()[0].model,
-  coreNum: os.cpus().length,
-  totalMemory: os.totalmem(),
-  nodeVersion: process.version,
-  os: os.platform() + ', ' + os.release(),
+const start = async () => {
+  const platform = {
+    cpu: os.cpus()[0].model,
+    coreNum: os.cpus().length,
+    totalMemory: os.totalmem(),
+    nodeVersion: process.version,
+    os: os.platform() + ', ' + os.release(),
+  };
+
+  const result = {
+    typescript: [],
+    scss: [],
+    nuxtGenerate: [],
+    lint: [],
+  };
+
+  const testList = [
+    {target: 'typescript', commands: 'cd example-ts && yarn run build'},
+    {target: 'scss', commands: 'cd example-scss && yarn run build'},
+    {target: 'nuxtGenerate', commands: 'cd example-nuxt && yarn run generate'},
+    {target: 'lint', commands: 'cd example-nuxt && yarn run lint'},
+  ];
+
+  for (let i = 0; i < STEP; i++) {
+    console.log(`start performance...[${i + 1}/${STEP}]`);
+
+
+    for (let j = 0; j < testList.length; j++) {
+      const item = testList[j];
+      console.log(`[${j + 1}/${testList.length}] ${item.target}...`);
+      const time = Date.now();
+      execSync(item.commands);
+      const ms = Date.now() - time;
+      result[item.target].push(ms);
+      await sleep(500);
+    }
+
+    await sleep(500);
+  }
+
+  console.log({
+    platform,
+    result,
+  });
 };
 
-
-const result = {
-  typescript: null,
-  scss: null,
-  nuxtGenerate: null,
-  lint: null,
-};
-
-const testList = [
-  {target: 'typescript', commands: 'cd example-ts && yarn run build'},
-  {target: 'scss', commands: 'cd example-scss && yarn run build'},
-  {target: 'nuxtGenerate', commands: 'cd example-nuxt && yarn run generate'},
-  {target: 'lint', commands: 'cd example-nuxt && yarn run lint'},
-];
-
-testList.forEach((item, index) => {
-  console.log(`[${index}/${testList.length}] ${item.target}...`);
-  const time = Date.now();
-  execSync(item.commands);
-  result[item.target] = Date.now() - time;
+const sleep = (ms) => new Promise(resolve => {
+  setTimeout(() => {
+    resolve();
+  }, ms);
 });
 
-
-console.log({
-  platform,
-  result,
-});
+start();
